169ddb11cbba0972f6091d99dba3c97e5725d87e
Olga <olyakapran@yandex.by>
2022-08-28 14:52:43 +0300

    third commit

---
+++ задача23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace задача23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.Write("Введите число: ");
+                int cube = Convert.ToInt32(Console.ReadLine());
+                int counter = 0;
+                int result = 0;
+
+                while(counter<=cube)
+                {
+                    Console.Write(result+", ");
+                    result =counter*counter*counter;
+                    counter++;
+                }
+            Console.WriteLine(result);
+        }
+    }
+}
